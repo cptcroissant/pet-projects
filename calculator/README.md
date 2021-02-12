e848a5379de95c7759385cb7cf341f6859dd1136
Kirill Klyukvin <kir.klyukvin@gmail.com>
2021-02-12 21:52:08 +0300

    brackets and exponentination added

--- calculator/README.md
+++ calculator/README.md
@@ -1,7 +1,37 @@
-# Python class Calculator
+# Calculator
 
-Python script. It gets an object - mathematical expression with numbers and operators, divided by spaces.   
-After computation class returns an answer of the expression in type float. All operations are executing according to math priority.  
-This version of script may perform three major types of operations: exponentiation, multiplications and division, addition and subtraction.  
-
-TODO: add the '()' operator 
+    
+    Python class - a simple calculator. 
+    
+    The function performs four fundamental operations, deals with brackets 
+    and may perform an exponentiation.
+    
+    The class takes an argument - a mathematical expression and returns the result in a type float.  
+    For example, object '( 2 + 3 ) / 2 ** 2' will be transformed into 1.25.  
+    
+    To make a calculation with the class use the following syntax:  
+    
+    ```
+    x = '( 2 + 3 ) / 2 ** 2'
+    q = Calculator(x)
+    q.calculation()
+    
+    >>> 1.25
+    ```
+    
+    The computation process performs four steps. First, if the input contains brackets,   
+    the function splits the string into special tokens, according to brackets operations queue.  
+    Next, we compute math operations under the brackets. 
+    After that, we combine changed tokens into a new expression and compute the rest operations. 
+    
+    Tokens may starts or ends with math symbols. These types of tokens are calling 'left' and 'right',  
+    according to a position of a special symbol. For example, token '/ 2 + 4' is the 'right one'.  
+    This approach is using during the performance of the right queue of math operations.
+    
+    In case without the brackets into the input expression, we skip the first step to reduce the computation time.  
+    
+    Class is sensitive for proper using the brackets. In some rare cases, 
+    the exponentiation of expression under the brackets may cause a wrong computation. 
+    Please, carefully check the places of brackets.
+    
+    Single spaces should split the characters in the input expression.
--- calculator/class_Calculator.py
+++ calculator/class_Calculator.py
@@ -1,29 +1,115 @@
 class Calculator():
     
     """
-    Python class - calculator
-    The class takes an argument - a mathematical expression and return the result in a type float.
+    Python class - a simple calculator. 
+    
+    The function performs four fundamental operations, deals with brackets 
+    and may perform an exponentiation.
+    
+    The class takes an argument - a mathematical expression and returns the result in a type float.  
+    For example, object '( 2 + 3 ) / 2 ** 2' will be transformed into 1.25.  
+    
+    To make a calculation with the class use the following syntax:  
+    
+    ```
+    x = '( 2 + 3 ) / 2 ** 2'
+    q = Calculator(x)
+    q.calculation()
+    
+    >>> 1.25
+    ```
+    
+    The computation process performs four steps. First, if the input contains brackets,   
+    the function splits the string into special tokens, according to brackets operations queue.  
+    Next, we compute math operations under the brackets. 
+    After that, we combine changed tokens into a new expression and compute the rest operations. 
+    
+    Tokens may starts or ends with math symbols. These types of tokens are calling 'left' and 'right',  
+    according to a position of a special symbol. For example, token '/ 2 + 4' is the 'right one'.  
+    This approach is using during the performance of the right queue of math operations.
+    
+    In case without the brackets into the input expression, we skip the first step to reduce the computation time.  
+    
+    Class is sensitive for proper using the brackets. In some rare cases, 
+    the exponentiation of expression under the brackets may cause a wrong computation. 
+    Please, carefully check the places of brackets.
+    
+    Single spaces should split the characters in the input expression.
+    
+    
+    Parameters
+    ----------
+    param_1 : str
+        A math expression in the special object form.
+   
+    Returns  
+    ------- 
+    float  
+        The result of the computation.
     """
     
     def __init__(self, string):
         
         self.string = string
         self.exp = None
+        self.bracket = None
         self.mult_div = None
         self.add_sub = None
         self.result = None
         
         if type(self.string) != str:
             raise ValueError('Wrong input type. Only str allowed')
+        
+        if self.string.count(' ') % 2 != 0:
+            raise ValueError('Check the spaces between the characters')
+        
     
     
+    # Function splits the input string into special tokens, according to brackets queue.
+    def brackets_split(self):
+
+        expr = self.string.split()  
+        
+        chars = '/-+*'
+        res, temp = [], []
+        br = 0
     
-    # Perform the exponential operation. The function may conduct several exponential operations one by one, just like  
-    # 2 ** 2 ** 2 without getting an error.
+        for i in range(len(expr)):
+        
+            if expr[i] == '(' and br == 0:
+                br += 1
+                temp = []             
+            elif expr[i] != '(' and br == 0:
+                res.append(expr[i])      
+            elif expr[i] == '(' and br > 0:
+                res.append(' '.join(temp))
+                temp = []
+                br += 1
+            elif  br > 0 and expr[i] != ')':
+                temp.append(expr[i])
+            elif expr[i] == ')':
+                res.append(' '.join(temp))
+                temp = []
+                br -= 1
+            
+        for i in reversed(range(len(res))):
+            if len(res[i]) == 0:
+                del res[i]
+        
+        print('brackets done', res)
+        
+        self.bracket = res
+        
+        
     
-    def exponent(self):
-    
-        self.exp = self.string.split()  
+    # The function performs the exponentiation. It deal with both '**' and '^' operands.
+    def exponent(self, arg=None):
+        
+        if arg is not None:
+            self.exp = arg.split()
+        else:
+            self.exp = self.string.split()  
+            
         res = []  
     
         for i in range(len(self.exp) - self.exp.count('**')):
@@ -35,16 +121,20 @@ class Calculator():
                 res.append(str(float(res[-1]) ** float(self.exp[i+1])))
                 del res[-2]
                 del self.exp[i]
-                
-        self.result = ' '.join(res)
-    
-    
-    
-    # Perform the multiplication and division operations.
-    
-    def mult_and_div(self):
         
-        if self.result is not None:
+        if arg is not None:
+            return ' '.join(res)
+        else:
+            self.result = ' '.join(res)
+
+    
+    
+    # The function performs multiplication and division. Only '*' and '/' math symbols allowed.
+    def mult_and_div(self, arg=None):
+        
+        if arg is not None:
+            self.mult_div = arg.split()
+        elif self.result is not None:
             self.mult_div = self.result.split()
         else:
             self.mult_div = self.string.split()
@@ -64,20 +154,24 @@ class Calculator():
         self.mult_div = list(filter(lambda a: a != '*', self.mult_div))
         self.mult_div = list(filter(lambda a: a != '/', self.mult_div))
         
-        self.mult_div = ' '.join(self.mult_div) 
-        self.result = self.mult_div       
+        if arg is not None:
+            return ' '.join(self.mult_div)
+        else:
+            self.mult_div = ' '.join(self.mult_div) 
+            self.result = self.mult_div       
  
 
 
-    # Perfomr the addition and subtraction operations.
-
-    def add_and_subs(self):
+    # The function performs addition and subtraction. Only '+' and '-' math symbols allowed.
+    def add_and_subs(self, arg=None):
     
-        if self.result is not None:
+        if arg is not None:
+            self.add_sub = arg.split()
+        elif self.result is not None:
             self.add_sub = self.result.split()
         else:
             self.add_sub = self.string.split()
-    
+        
         res = float(self.add_sub[0])
         
         for i in range(1, len(self.add_sub)-1):
@@ -85,26 +179,93 @@ class Calculator():
                 res += float(self.add_sub[i+1])
             if self.add_sub[i] == '-':
                 res -= float(self.add_sub[i+1])
-            
-        self.result = res
+                
+        if arg is not None:
+            return res
+        else:
+            self.result = res
         
-        
-        
-    # Main function. It performs three types of math operations sequentially.
-    # To reduce computation time, first, we check the input expression for exponential,
-    # addition and division operations. 
-    # If non of them exists, we perform only addition or subtraction operations.
     
+    
+    # The main calculation procedure
     def calculation(self):
-        
-        if self.string.count('**') > 0:
-            self.exponent()
-        if self.string.count('*') > 0 or self.string.count('/') > 0:
-            self.mult_and_div()
-        self.add_and_subs()
-        
-        return self.result
-    
-    
-    #def __repr__(self):
-        #return '{}'.format(self.calculation())
+         
+        #in case of brackets in input expression performs the long way
+        if self.string.count('(') > 0:
+            self.brackets_split()
+            res, temp = [], []
+            chars = '/*+-'
+            for i in range(len(self.bracket)):
+                if len(self.bracket[i]) > 1 and self.bracket[i][0] not in chars and self.bracket[i][-1] not in chars:
+                    exp = self.exponent(arg=self.bracket[i])
+                    m_d = self.mult_and_div(arg=exp)
+                    a_s = self.add_and_subs(arg=m_d)
+                    res.append(str(a_s))
+                else:
+                    res.append(self.bracket[i]) 
+            print('calc brackets {}'.format(res))
+            
+            #perform exponentiation of expression under the breakets        
+            for i in range(len(res)):
+                if res[i][:2] == '**' and len(res[i]) > 3:              
+                    temp.pop()
+                    unit = res[i-1] + ' ' + res[i]
+                    exp = self.exponent(arg=unit)
+                    m_d = self.mult_and_div(arg=exp)
+                    a_s = self.add_and_subs(arg=m_d)
+                    temp.append(str(a_s))                
+                else:
+                    temp.append(res[i])
+                    
+            res = temp
+            #print('exp.done', res)
+            temp = []
+            
+            # computing the 'right side' operations
+            for i in range(len(res)):
+                if res[i][0] in chars and len(res[i]) > 2:       
+                    unit = temp[-1] + ' ' + res[i]
+                    temp.pop()
+                    exp = self.exponent(arg=unit)
+                    m_d = self.mult_and_div(arg=exp)
+                    a_s = self.add_and_subs(arg=m_d)
+                    temp.append(str(a_s))  
+                else:
+                    temp.append(res[i])
+            
+            res = temp
+            temp = []
+            #print('rigth_done', res)
+            
+            # computing the 'left side' operations
+            for i in reversed(range(len(res))):
+                if res[i][-1] in chars and len(res[i]) > 2: 
+                    unit = res[i] + ' ' + temp[-1] 
+                    temp.pop()
+                    exp = self.exponent(arg=unit)
+                    m_d = self.mult_and_div(arg=exp)
+                    a_s = self.add_and_subs(arg=m_d)
+                    temp.append(str(a_s)) 
+                else:
+                    temp.append(res[i])
+            
+            temp = temp[::-1]
+            res = ' '.join(temp)
+            temp = []
+            #print('left_done', res)
+            
+            temp = self.exponent(arg=res)
+            temp = self.mult_and_div(arg=temp)
+            self.result = self.add_and_subs(arg=temp)
+            
+            return self.result
+         
+        #if there are no brackets, only three steps needed
+        else:
+            if self.string.count('**') > 0:
+                self.exponent()
+            if self.string.count('*') > 0 or self.string.count('/') > 0:
+                self.mult_and_div()
+            self.add_and_subs()
+            
+            return self.result
